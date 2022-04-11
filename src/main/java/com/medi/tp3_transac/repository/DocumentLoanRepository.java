b52770e68327774dd0973d6755fc2817719f2f8a
TeddyGuy <mediteddyguy@gmail.com>
2022-04-11 00:09:36 -0400

    Creation de la class repository DocumentLoanRepository

---
+++ src/main/java/com/medi/tp3_transac/repository/DocumentLoanRepository.java
@@ -0,0 +1,7 @@
+package com.medi.tp3_transac.repository;
+
+import com.medi.tp3_transac.model.DocumentLoan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentLoanRepository extends JpaRepository<DocumentLoan, Long> {
+}
