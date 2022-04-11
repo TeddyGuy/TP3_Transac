ee6cf42e1d4a664ff363c643ef9d6b793b08a6ee
TeddyGuy <mediteddyguy@gmail.com>
2022-04-11 01:26:11 -0400

    Creation de la methode DocumentRepository.findByTitleAndAuthorAndGenreAndPublicationYear

--- src/main/java/com/medi/tp3_transac/repository/DocumentRepository.java
+++ src/main/java/com/medi/tp3_transac/repository/DocumentRepository.java
@@ -2,6 +2,20 @@ package com.medi.tp3_transac.repository;
 
 import com.medi.tp3_transac.model.document.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+    @Query("select d " +
+            "from Document d " +
+            "where LOWER(d.title) like %:titleSearch% " +
+            "and d.author like %:authorSearch% " +
+            "and d.genre like %:genreSearch% " +
+            "and d.publicationYear = :publicationYearSearch")
+    List<Document> findByTitleAndAuthorAndGenreAndPublicationYear(@Param("titleSearch") String titleSearch,
+                                                                  @Param("authorSearch") String author,
+                                                                  @Param("genreSearch") String genre,
+                                                                  @Param("publicationYearSearch") int publicationYear);
 }
