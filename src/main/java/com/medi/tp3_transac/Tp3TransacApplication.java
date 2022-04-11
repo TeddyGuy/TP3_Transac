73e683c67c120ac9972f5a862402a98653dc4c5c
TeddyGuy <mediteddyguy@gmail.com>
2022-04-10 21:36:47 -0400

    Initialisation du projet

---
+++ src/main/java/com/medi/tp3_transac/Tp3TransacApplication.java
@@ -0,0 +1,13 @@
+package com.medi.tp3_transac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3TransacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3TransacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medi/tp3_transac/Tp3TransacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi.tp3_transac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3TransacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
