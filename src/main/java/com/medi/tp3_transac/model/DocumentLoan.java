5f3910d93243b7995ba7fbda4b9753d6710d264b
TeddyGuy <mediteddyguy@gmail.com>
2022-04-10 21:51:42 -0400

    Creation de la class model DocumentLoan

---
+++ src/main/java/com/medi/tp3_transac/model/DocumentLoan.java
@@ -0,0 +1,28 @@
+package com.medi.tp3_transac.model;
+
+
+import lombok.*;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class DocumentLoan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    //@ManyToOne
+    //@JoinColumn(name = "document_id")
+    //@ToString.Exclude
+    //private Document document;
+    private LocalDate lendingDate;
+    private LocalDate expectedReturnDate;
+    //@ManyToOne
+    //@JoinColumn(name = "client_id")
+    //@ToString.Exclude
+    //private Client client;
+}
