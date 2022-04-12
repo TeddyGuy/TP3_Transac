b3ce355269be2864aaf9108b81d71001332c6675
Teddy Guy <mediteddyguy@gmail.com>
2022-04-11 21:07:40 -0400

    implementation de la route "/"

--- src/main/java/com/medi/tp3_transac/controller/AdminController.java
+++ src/main/java/com/medi/tp3_transac/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.medi.tp3_transac.controller;
 
 import com.medi.tp3_transac.service.AdminService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
@@ -9,5 +11,11 @@ public class AdminController {
 
     public AdminController(AdminService adminService){
         this.adminService = adminService;
+    }
+
+    @GetMapping("/")
+    public String getRootRequest(Model model){
+        model.addAttribute("pageTitle","JavaTown Library System");
+        return "index";
     }
 }
